89bb2e1b90b74db885ab370c90fde65796ea63af
AhmedAdel208 <ahmed49adel90@gmail.com>
2024-05-18 14:52:49 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Navbar from "./components/navbar/Navbar";
+import Header from "./containers/header/Header";
+import Brand from "./components/brand/Brand";
+import WhatGPT3 from "./containers/whatgpt3/Whatgpt3";
+import Features from "./containers/features/Features";
+import Possibility from "./containers/possibility/Possibility";
+import CTA from "./components/cta/Cta";
+import Blog from "./containers/blog/Blog";
+import Footer from "./containers/footer/Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
